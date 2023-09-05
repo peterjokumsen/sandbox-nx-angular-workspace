27a5951c58ed9bcdc157664c96746b1d61da52bf
Peter Jokumsen <pjokumsen@gmail.com>
2023-09-05 22:54:02 +0200

    chore: install @nx-squeezer/workspace:prettier
    Including formatting changes applied from new default rules.

--- apps/client-app/src/app/app.component.ts
+++ apps/client-app/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NgOptimizedImage } from "@angular/common";
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   standalone: true,
